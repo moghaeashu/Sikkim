fc8aeae9b831a14209331b68c8c6676ddd05397d
Harshit Mogha <moghaeashu@gmail.com>
2024-09-21 15:48:34 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -22,3 +22,28 @@ window.addEventListener("wheel",function(dets){
         })
     }
 })
+
+gsap.to(".mobilelogos img",{
+    transform:"translateX(-2000%)",
+    duration:40,
+    repeat:-1,
+    ease:"none"
+})
+
+window.addEventListener("wheel",function(dets){
+    if(dets.deltaY>0){
+        gsap.to(".mobilelogos img",{
+            transform:"translateX(-2000%)",
+            duration:40,
+            repeat:-1,
+            ease:"none"
+        })
+    } else {
+        gsap.to(".mobilelogos img",{
+            transform:"translateX(2000%)",
+            duration:40,
+            repeat:-1,
+            ease:"none"
+        })
+    }
+})
